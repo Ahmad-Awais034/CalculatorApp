08b69ff422bc9612b8f66e37a16242dbba95d01a
ahmad880 <aahhmmaadd.bkr@gmail.com>
2022-11-27 00:00:17 +0500

    Method for addition

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Add(View v){
+        EditText et1=(EditText)findViewById(R.id.editTextNumber);
+        EditText et2=(EditText)findViewById(R.id.editTextNumber2);
+        EditText et3=(EditText)findViewById(R.id.editTextNumber3);
+        int n1=Integer.parseInt(et1.getText().toString());
+        int n2=Integer.parseInt(et2.getText().toString());
+        int result=n1+n2;
+        et3.setText("Total value"+result);
     }
 }
