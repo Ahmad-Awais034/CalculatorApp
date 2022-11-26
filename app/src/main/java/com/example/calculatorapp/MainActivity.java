d734e732bd1b58c0f46fa3a663d57fda1acfec62
ahmad880 <aahhmmaadd.bkr@gmail.com>
2022-11-27 00:02:42 +0500

    Method for subtraction

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -22,4 +22,14 @@ public class MainActivity extends AppCompatActivity {
         int result=n1+n2;
         et3.setText("Total value"+result);
     }
+    public void Subtract(View v){
+        EditText et1=(EditText)findViewById(R.id.editTextNumber);
+        EditText et2=(EditText)findViewById(R.id.editTextNumber2);
+        EditText et3=(EditText)findViewById(R.id.editTextNumber3);
+        int n1=Integer.parseInt(et1.getText().toString());
+        int n2=Integer.parseInt(et2.getText().toString());
+        int result=n1-n2;
+        et3.setText("Subtracted value"+result);
+    }
+
 }
