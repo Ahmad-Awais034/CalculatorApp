67d5a22414f8ac72378a6b8ea6e1f215bbfb76c5
ahmad880 <aahhmmaadd.bkr@gmail.com>
2022-11-27 00:05:22 +0500

    Method for multiplication and division

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -31,5 +31,23 @@ public class MainActivity extends AppCompatActivity {
         int result=n1-n2;
         et3.setText("Subtracted value"+result);
     }
+    public void Multiply(View v){
+        EditText et1=(EditText)findViewById(R.id.editTextNumber);
+        EditText et2=(EditText)findViewById(R.id.editTextNumber2);
+        EditText et3=(EditText)findViewById(R.id.editTextNumber3);
+        int n1=Integer.parseInt(et1.getText().toString());
+        int n2=Integer.parseInt(et2.getText().toString());
+        int result=n1*n2;
+        et3.setText("Multiplied value"+result);
+    }
+    public void Divide(View v){
+        EditText et1=(EditText)findViewById(R.id.editTextNumber);
+        EditText et2=(EditText)findViewById(R.id.editTextNumber2);
+        EditText et3=(EditText)findViewById(R.id.editTextNumber3);
+        int n1=Integer.parseInt(et1.getText().toString());
+        int n2=Integer.parseInt(et2.getText().toString());
+        int result=n1/n2;
+        et3.setText("Divided value"+result);
+    }
 
 }
